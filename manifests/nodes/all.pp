62b515aa8e01d6ff780505b024cccdadec2ac61c
Matt Reishus <mattr@lavabit.com>
2013-03-23 16:36:30 +0000

    Add UFW firewall module

--- manifests/nodes/all.pp
+++ manifests/nodes/all.pp
@@ -1,4 +1,16 @@
 node default {
     class { "minimal": }
     class { "etckeeper": }
+
+    # Firewall
+    include ufw
+    ufw::allow { "allow-ssh-from-all":
+        port => 22,
+    }
+    ufw::allow { "allow-http-from-all":
+        port => 80,
+    }
+    ufw::allow { "allow-https-from-all":
+        port => 443,
+    }
 }
